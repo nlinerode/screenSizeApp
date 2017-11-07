bc706c76ad8fbba90a5da75e59b732081561b691
Nathan Linerode <nlinerode2@gmail.com>
2017-11-07 12:27:23 -0500

    first commit, add index, css, js

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'public'
+    }
+  })
+});
+
+gulp.task('sass', function(){
+  return gulp.src('public/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.reload({
+      stream:true
+    }))
+});
+
+gulp.task('useref', function(){
+  return gulp.src('public/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('public/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// gulp.task('fonts', function(){
+//   return gulp.src('public/fonts/**/*')
+//     .pipe(gulp.dest('dist/fonts'));
+// });
+
+gulp.task('clean:dist', function(){
+  return del.sync('dist');
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('public/scss/**/*.scss', ['sass']);
+  gulp.watch('public/*.html', browserSync.reload);
+  gulp.watch('public/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('default', function(callback){
+  runSequence(['sass', 'browserSync', 'watch'], callback)
+});
+
+gulp.task('build', function(callback){
+  runSequence('clean:dist', ['sass', 'useref', 'images'], callback)
+});
